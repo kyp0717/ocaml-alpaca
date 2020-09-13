98fb8a00ba304468bb500566a619f88722fb1602
phage <kyp0717@gmail.com>
2020-09-13 12:08:13 -0400

    extract values from yaml file

--- testyaml.ml
+++ testyaml.ml
@@ -15,19 +15,27 @@ let get_list  (yaml: Yaml.value) = match yaml with
  | `O assoc -> Some assoc
  | _ -> None
 
-
-let yaml = Yaml_unix.of_file_exn (Fpath.v "keys.paper.yaml") 
-
 let unbox opt = match opt with
   | Some x -> x
-  | None -> print_endline "No Value"
+  | None -> [("null", `String "No key found")]
+
+let my_value (s:Yaml.value) = match s with
+  | `String x -> x
+  | _ -> " "
 
 let () =
-  let yaml = Yaml_unix.of_file_exn (Fpath.v "keys.paper.yaml") in
-  let val1a = get_value "key" yaml in
-  (* let val1b = unbox val1a in *)
-  print_endline (val1a)
+  let yaml = Yaml_unix.of_file_exn (Fpath.v "testyaml.yml") in
+  let x1 = get_list yaml in
+  let x2 = unbox x1 in
+  let x3 = List.assoc "key" x2 in 
+  let x4 = my_value x3 in
+  print_endline (x4)
 
-let l = get_list yaml
+let yaml = Yaml_unix.of_file_exn (Fpath.v "testyaml.yml") 
+let x1 = get_list yaml 
+let x2 = unbox x1 
+let x3 = List.assoc "key" x2  
+let x4 = my_value x3 
 
-let l2 = unbox l
+
+_
